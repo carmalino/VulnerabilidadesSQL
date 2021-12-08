05495975465b3910cc51d17ccd01caaab7068ac8
mcarvajal <carmalino@gmail.com>
2021-12-08 18:42:43 -0500

    Creacion Noticias

---
+++ VulnerabilidadesSQL.API/Data/Entities/New.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VulnerabilidadesSQL.API.Data.Entities
+{
+    public class New
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Titulo")]
+        [MaxLength(50,ErrorMessage ="El campo {0} no puede tener más de {1} carácteres")]
+        [Required(ErrorMessage ="El titulo es obligatorio")]
+        public string Title { get; set; }
+
+        [Display(Name = "Mensaje")]
+        [Required(ErrorMessage = "El mensaje es obligatorio")]
+        public string Body { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
